3ac55467b75997a2ee65a08c02c2e8aaa2afa373
shoaib-ahmed16 <shoaib-ahmed16>
2024-03-18 08:19:58 +0530

    Adding the changes

---
+++ src/main/java/com/elevatemart/elevatemartapigateway/config/LoadBalancerRoutes.java
@@ -0,0 +1,30 @@
+package com.elevatemart.elevatemartapigateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile("local-discovery")
+@Configuration
+public class LoadBalancerRoutes {
+    @Bean
+    public RouteLocator loadBalancerRoutes(RouteLocatorBuilder builder){
+        return builder.routes()
+                .route("security-service", r -> r.path("/api/v1/security/**")
+                        .uri("lb://security-micro-service"))
+                .route("cart-service", r -> r.path("/api/v1/cart/**")
+                        .uri("lb://cart-micro-service"))
+                .route("product-service", r -> r.path("/api/v1/attribute/**", "/api/v1/category/**", "/api/v1/product/**", "/api/v1/tax/**")
+                        .filters(
+                                f->f.circuitBreaker(c->c.setName("productCB")
+                                    .setFallbackUri("forward:/product-failOver-micro-service")
+                                    .setRouteId("product-failOver-fallback-id")
+                                ))
+                        .uri("lb://product-micro-service"))
+                .route("product-failOver-id", r -> r.path("/product-failOver-micro-service/**")
+                        .uri("lb://product-failOver-micro-service"))
+                .build();
+    }
+}
